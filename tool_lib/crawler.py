fc72dd526eca020671b88ff3ebc5375c370c104b
lolichan6304 <lolichan6304@gmail.com>
2020-07-30 16:22:16 +0800

    added downloader

--- tool_lib/crawler.py
+++ tool_lib/crawler.py
@@ -1,6 +1,9 @@
 from lxml import html
 import requests
 import os
+import numpy as np
+import time
+import urllib.request as urllib_request
 
 GLOBAL_AMCOMIC = 'https://www.amcomic.com'
 
@@ -10,7 +13,7 @@ def filter_text(text):
             text = text.replace(ch,"")
     return text
 
-def amcomic_crawler(chap_code='/chapter/9698', code_dir='./codes'):
+def amcomic_crawler(chap_code='/chapter/9698', code_dir='./codes', save=True):
     url = GLOBAL_AMCOMIC + chap_code
     print("populating from: {}".format(url))
     page = requests.get(url)
@@ -26,19 +29,72 @@ def amcomic_crawler(chap_code='/chapter/9698', code_dir='./codes'):
     print("found {} chapters".format(len(chapters)))
 
     codes = []
+    result = []
     if not os.path.exists(os.path.join(code_dir,dst)):
-        result = []
         for chapter in chapters:
             new_url = GLOBAL_AMCOMIC + chapter.attrib['href']
             result.append(new_url)
             codes.append(int(chapter.attrib['href'].split('/')[-1]))
 
-        with open(os.path.join(code_dir,dst), 'w') as filehandle:
-            filehandle.writelines("%s\n" % chap for chap in result)
+        if save:
+            with open(os.path.join(code_dir,dst), 'w') as filehandle:
+                filehandle.writelines("%s\n" % chap for chap in result)
     else:
         for chapter in chapters:
             codes.append(int(chapter.attrib['href'].split('/')[-1]))
         print("file already exists!")
 
-    return codes
+    return codes, result
+
+def download_from_amcomic(url, save_dir):
+    print("download from {}".format(url))
+    page = requests.get(url)
+    print("successfully connected to url")
+    tree = html.fromstring(page.content)
+
+    print("="*10)
+    # get title
+    title = tree.xpath('//head/title/text()')[0]
+    print("title: {}".format(title))
+    save_to = os.path.join(save_dir, title)
+    if not os.path.exists(save_to): # file has not been processed before
+        print("folder not found in {}, creating folder...".format(save_to))
+        os.mkdir(save_to)
+    images = tree.xpath('//div[@class="comiclist"]/div/img')
+    print("found {} pages".format(len(images)))
+    if len(images) > 4:
+        print("downloading...")
+        for i, page in enumerate(images):
+            p_url = page.attrib['src']
+            urllib_request.urlretrieve(p_url, os.path.join(save_to, "pic_{:03d}.jpg".format(i)))
+
+    print("="*10)
+    print("save to: {}".format(save_dir))
+    slp = np.random.uniform(2,5)
+    print("sleeping for {:.4f} secs...".format(slp))
+    time.sleep(slp)
+
+def amcomic_downloader(code_dir='./check_updates', save_dir='./amcomic'):
+    opener = urllib_request.build_opener()
+    opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+    urllib_request.install_opener(opener)
+    
+    for codes in os.listdir(code_dir):
+        print("checking {}".format(codes))
+        urls = [l.strip('\n\r') for l in open(os.path.join(code_dir,codes), 'r').readlines()]
+        print("number of chapters: {}".format(len(urls)))
+
+        dst = os.path.join(save_dir, codes[:-4])
+        if not os.path.exists(dst): # file has not been processed before
+            print("folder not found in {}, creating folder...".format(save_dir))
+            os.mkdir(dst)
+        folder = sorted(os.listdir(dst))
+        print("found {} folders".format(len(folder)))
+        code = '/chapter/{}'.format(urls[0].split('/')[-1])
+        _, result = amcomic_crawler(code, '', False)
+        #print("currently there are {} chapters".format(len(result)))
+        if len(result) > len(folder): # here we need to download the files
+            for index in range(len(folder), len(result)):
+                download_from_amcomic(result[index], dst)
+
         
---
+++ tool_lib/tesseract.py
@@ -0,0 +1,61 @@
+import pytesseract
+from pytesseract import Output
+import cv2
+from PIL import Image
+import numpy as np
+import os
+
+accepted_lang = ["kor", "chi_tra"]
+
+def ocr(database, compiled, lang):
+    for folder in sorted(os.listdir(database)):
+        for chapter in os.listdir(os.path.join(database,folder)):
+            print("working on folder: {}".format(chapter))
+            # check directory in compiled
+            list_pages = sorted(os.listdir(os.path.join(compiled, folder, chapter)))
+            # Check if the chapter is cropped in compiled
+            if len(list_pages) > 0:
+                print("folder has been cropped! moving to next folder")
+            else:
+                list_pages = sorted(os.listdir(os.path.join(database, folder, chapter)))
+                print("number of pages: {}".format(len(list_pages)))
+                for i in list_pages:
+                    img = Image.open(os.path.join(database, folder, chapter, i)).convert('L')
+                    img.save(os.path.join(compiled, folder, chapter, i), dpi=(300, 300))
+                    img = cv2.imread(os.path.join(compiled, folder, chapter, i), 0)
+                    #img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2)
+                    img = cv2.threshold(img,10,255,cv2.THRESH_BINARY)[1]
+                    d = pytesseract.image_to_data(img, lang=lang, config='--psm 1 --oem 3', output_type=Output.DICT)
+                    text = pytesseract.image_to_string(img, lang=lang, config='--psm 1 --oem 3')
+                    n_boxes = len(d['level'])
+                    for j in range(n_boxes):
+                        if d['level'][j] == 5:
+                            (x, y, w, h) = (d['left'][j], d['top'][j], d['width'][j], d['height'][j])
+                            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                    cv2.imwrite(os.path.join(compiled, folder, chapter, i), img)
+                    with open(os.path.join(compiled, folder, chapter, i[:-4]+".txt"), "w", 5, "utf-8") as text_file:
+                        text_file.write(text)
+                        text_file.close()
+
+
+'''file_path = './database/Ex Wife/TOPTOON_頂通-國際官方中文版-韓國最新漫畫-線上免費看_-_與前妻同居_-_第1話/pic_0{}.jpg'
+for j in range(10, 32):
+    im = Image.open(file_path.format(j)).convert('RGB')
+    im.save('ocr.png', dpi=(300, 300))
+    image = cv2.imread('ocr.png')
+    d = pytesseract.image_to_data(image, lang="chi_tra", output_type=Output.DICT)
+    print(d)
+    n_boxes = len(d['level'])
+    for i in range(n_boxes):
+        if d['level'][i] == 4:
+            print(d['text'][i])
+            (x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])
+            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    cv2.imwrite("output_{}.png".format(j), image)
+    """cv2.imshow('img', image)
+    cv2.waitKey(0)"""
+    image = cv2.imread('ocr.png')
+    text = pytesseract.image_to_string(image, lang="chi_tra")
+    with open("output_{}.txt".format(j), "w", 5, "utf-8") as text_file:
+        text_file.write(text)
+        text_file.close()'''
